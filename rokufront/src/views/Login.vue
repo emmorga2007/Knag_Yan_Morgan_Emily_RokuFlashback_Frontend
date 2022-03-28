cfe8e5403efc8dc4e4de55fa595651a3c60b2bf7
Emily Morgan <emily189morgan@hotmail.com>
2022-03-27 23:14:16 -0400

    front end userselect

--- rokufront/src/views/Login.vue
+++ rokufront/src/views/Login.vue
@@ -9,15 +9,16 @@
 
     <section class="log-in">
       <label class="sr-only" for="inlineFormInputName">Name</label>
-      <input type="text" class="form-control" id="inlineFormInputName" placeholder="username" required>
+      <input v-model="username" type="text" class="form-control" id="inlineFormInputName" placeholder="username" required>
 
       <label class="sr-only" for="inlineFormPassword">Name</label>
-      <input type="password" class="form-control" id="inlineFormPassword" placeholder="password" required>
+      <input v-model="password" type="password" class="form-control" id="inlineFormPassword" placeholder="password" required>
     </section>
 
     <button
         type="submit" 
         class="btn btn-primary login-submit"
+        @click="tryLogin"
       >Go!
     </button>
   </section>
@@ -26,6 +27,40 @@
 <script>
 export default {
   name: 'TheLogin',
+
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    tryLogin() {
+      //and use the rout to show the next view 
+      //TODO: this should only happen on a successful login -> this needs to move to the fetch call
+      this.$router.push({ name: 'UserSelect' }) 
+
+      //mock up the login functionality -> form data is a virtual <form> element's data
+      /*let formData = new FormData();
+
+      formData.append("username", this.username);
+      formData.appened("password", this.password);
+
+      //pass the form data to out login API (we dont know what this will be yet. nicks class is foing it.)
+      let url = 'someloginurl';
+
+      fetch(url, {
+        method: 'POST',
+        body: formData
+      })
+      .then(res => res.json())
+      .then(data => console.table(data))
+      .catch(err => console.error(err)); */
+    }
+  }
+
+
 }
 </script>
 
